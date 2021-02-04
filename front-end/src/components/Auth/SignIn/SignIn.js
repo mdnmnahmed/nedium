d962645fafb28066217837575ae1f4a307218f30
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2021-02-04 20:29:25 +0530

    added

---
+++ front-end/src/components/Auth/SignIn/SignIn.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SignIn = () => {
+    return (
+        <div className="sign-container">
+            <h1>Sign In</h1>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
+
+export default SignIn
