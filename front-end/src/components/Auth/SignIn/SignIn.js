d0ed1d861fb5d1e1732dd69224d81485851ec44e
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2021-03-06 18:25:18 +0530

    login page build

--- front-end/src/components/Auth/SignIn/SignIn.js
+++ front-end/src/components/Auth/SignIn/SignIn.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 const SignIn = () => {
-    const[email,]
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submit clicked')
+    }
     return (
         <div className="sign-container">
             <h1>Sign In</h1>
             <form onSubmit={handleSubmit}>
-                <input type="email" name="email" value={email}/>
-                <input type="password" name="password" value={password}/>
+                <input type="email" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
                 <button type="submit">Submit</button>
             </form>
         </div>
